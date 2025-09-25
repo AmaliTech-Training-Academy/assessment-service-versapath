0039cabf35fb81e4164953b57821004d49bee96f
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-09-25 19:44:33 +0200

    chore: add dependencies

--- src/main/java/com/capstone/assessment_service/AssessmentServiceApplication.java
+++ src/main/java/com/capstone/assessment_service/AssessmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.capstone.assessment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AssessmentServiceApplication {
 
 	public static void main(String[] args) {
