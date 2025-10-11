c0e08e83021292190e5597b9b961fdd0c65a2368
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-10-11 14:05:59 +0200

    feat: listen to an event to persist user info in database

---
+++ src/main/java/com/capstone/assessment_service/messaging/CreateUserListener.java
@@ -0,0 +1,19 @@
+package com.capstone.assessment_service.messaging;
+
+import lombok.RequiredArgsConstructor;
+import org.common.event.ProduceUserEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateUserListener {
+    private static final Logger logger = LoggerFactory.getLogger(CreateUserListener.class);
+    @KafkaListener(topics = "${USER_CREATE_TOPIC}")
+
+    public void createUser(ProduceUserEvent event) {
+        logger.info("Start creating user event {}", event);
+    }
+}
