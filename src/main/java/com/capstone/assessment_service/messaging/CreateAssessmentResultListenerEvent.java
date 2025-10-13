f2ffbe7ab29c31d86d3f8c464c63e08605f9cbd6
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-10-13 10:48:46 +0200

    feat: listen to assessment result event to start inserting to db

---
+++ src/main/java/com/capstone/assessment_service/messaging/CreateAssessmentResultListenerEvent.java
@@ -0,0 +1,22 @@
+package com.capstone.assessment_service.messaging;
+
+import com.capstone.assessment_service.service.AssessmentService;
+import lombok.RequiredArgsConstructor;
+import org.common.event.AssessmentResultEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CreateAssessmentResultListenerEvent {
+    private static final Logger logger = LoggerFactory.getLogger(CreateAssessmentResultListenerEvent.class);
+    private final AssessmentService assessmentService;
+
+    @RabbitListener(queues = "${ASSESSMENT_RESULT_QUEUE}")
+    public void handleMoodleUserCreation(AssessmentResultEvent event) {
+        logger.info("Start inserting assessment result to the database: {}", event);
+
+    }
+}
